466c7d04d04bb1ce30df4142bdfaba92e34718d8
JoaoPedro <joaohulck@gmail.com>
2024-10-21 18:16:42 -0300

    updt

--- programacao-estruturada/p1/ex2.c
+++ programacao-estruturada/p1/ex2.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
 
-void fill_array(int** x, int n);
-void print_array(int** x);
+void fill_array(int* x, int n);
+void print_array(int* x, int n);
 
 int main(void)
 {
     int n;
     scanf("%d", &n);
     int* array[n*2];
-    int* arrayf[n];
-    int* arrayb[n];
+    int arrayf[n];
+    int arrayb[n];
     fill_array(arrayf, n);
     fill_array(arrayb, n);
     printf("F:");
-    print_array(arrayf);
+    print_array(arrayf, n);
+    printf("B:");
+    print_array(arrayb, n);
 }
 
 
-void fill_array(int** x, int n)
+void fill_array(int* x, int n)
 {
     for (int i = 0; i < n; i++)
     {
@@ -25,13 +27,17 @@ void fill_array(int** x, int n)
     }
 }
 
-void print_array(int** x)
+void print_array(int* x, int n)
 {
-    int n = sizeof(x);
     for (int i = 0; i < n; i++)
     {
         printf(" %i", x[i]);
     }
     printf("\n");
+}
+
+void sort_array(int* x, int n)
+{
+    int xsorted[n]
 }
 
