5d9b5d989330572c8063a4f28c7801935f9e08c8
JoaoPedro <joaohulck@gmail.com>
2024-10-22 14:51:59 -0300

    updt

--- Project-Euler/ex.c
+++ Project-Euler/ex.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-
-#define maxWords 7
-#define maxLetters 20
 
 int main() {
-    char* words;
-    FILE* example = fopen("./assets/ex.txt", "r");
+    FILE* example = fopen("./assets/22.txt", "r");
     if (example == NULL)
     {
         printf("Error: could not open file.");
         return 1;
     }
 
+    int expected = 6000;
+    int standLetters = 20;
     int c;
     int w = 0;
     char l = 0;
-    char names[maxWords][maxLetters];
+    char** names = malloc(expected * sizeof(char*));
+    for (int i = 0; i < expected; i++)
+    {
+        names[i] = malloc(standLetters * sizeof(char));
+    }
+
     while ((c = fgetc(example)) != EOF)
     {
        if (c >= 'A' && c <= 'Z')
@@ -24,7 +28,7 @@ int main() {
             names[w][l] = c;
             l += 1;
         }
-        else if (c == ' ')
+        else if (c == ',')
         {
             names[w][l+1] = '\0';
             w += 1;
@@ -32,17 +36,18 @@ int main() {
         }
     }
 
-    names[w][l+1] = '\0';
-    names[w+1][0] = '\0';
+    names = realloc(names, w * sizeof(char*));
 
-
-    for (int i = 0; i < 5; i++)
+    printf("%s", names[0]);
+    for (int i = 1; i <= w; i++)
     {
-        printf("%s, ", names[i]);
+        printf(", %s", names[i]);
     }
 
-    
+    for (int i = 0; i < w; i++) {
+        free(names[i]);
+    }
+    free(names);
     fclose(example);
-
     return 0;
 }
