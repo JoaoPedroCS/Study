5a5dc4b826997cd85164cb1a2daa20682d22b3a5
JoaoPedro <joaohulck@gmail.com>
2024-10-23 19:34:31 -0300

    updt

---
+++ Project-Euler/23.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+void Xdivisors(double x);
+double sqrt(double x);
+
+int main(void)
+{
+    
+}
+
+void Xdivisors(double x)
+{
+    int rounded = (int) sqrt(x);
+}
+
+double sqrt(double x)
+{
+    if (x < 0) {
+        printf("Negative input error\n");
+        return -1;
+    }
+    
+    double tolerance = 0.000001;
+    double guess = x / 2.0;
+    if (x >= 1000000)
+    {
+        double guess = x / 1000000.0;
+    }
+    else if (x >= 10000)
+    {
+        double guess = x / 100.0;
+    }
+    else if (x >= 100)
+    {
+        double guess = x / 10.0;
+    }
+    
+    while ((guess * guess - x) > tolerance || (guess * guess - x) < -tolerance) {
+        guess = (guess + x / guess) / 2.0;
+    }
+    
+    return guess;
+}
+
+
+
